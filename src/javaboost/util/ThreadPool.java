965e9398c69cda58ce1f43d67f1ddc8a36d88fba
Kevin Shih <kevin.j.shih@gmail.com>
2012-02-06 22:20:52 -0500

    added singleton construct for threadpool

---
+++ src/javaboost/util/ThreadPool.java
@@ -0,0 +1,32 @@
+package javaboost.util;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPool{
+    private static ExecutorService m_instance = null;
+    private static int m_numThreads = 0;
+
+    public static synchronized ExecutorService getThreadpoolInstance() {
+	if (m_instance == null) {
+	    m_numThreads = Math.min(1, Runtime.getRuntime().availableProcessors()-1);
+	    m_instance = Executors.newFixedThreadPool(m_numThreads);
+	}
+	return m_instance;
+    }
+
+    public static synchronized ExecutorService getThreadpoolInstance(int numThreads) {
+	if(m_instance == null) {
+	    m_instance = Executors.newFixedThreadPool(numThreads);
+	} else if(numThreads != m_numThreads) {
+	    // if not the right number of threads
+	    m_instance.shutdown(); //shutdown
+	    m_instance = Executors.newFixedThreadPool(numThreads);
+	}
+
+	m_numThreads = numThreads;
+	return m_instance;
+    }
+
+}
