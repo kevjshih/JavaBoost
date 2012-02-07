ebdc9f6f91fc1582dbb947f34397b7a6ffc4ec9a
Kevin Shih <kevin.j.shih@gmail.com>
2012-02-06 22:47:58 -0500

    hopefully fixing additive classifier addvector issue

--- src/javaboost/boosting/AdditiveClassifier.java
+++ src/javaboost/boosting/AdditiveClassifier.java
@@ -13,6 +13,9 @@ public class AdditiveClassifier{
 
     public double[] classify(float[][] data) {
 	double[] output = new double[data.length];
+	for(int i = 0; i < data.length; ++i) {
+	    output[i] = 0;
+	}
 	for(WeakClassifier wc: m_classifiers) {
 	    output = Utils.addVectors(output, wc.classify(data));
 	}
