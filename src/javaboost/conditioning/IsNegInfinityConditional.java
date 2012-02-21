0d8b5776ae9b6dd63f8be479c2e932c6aa514978
Kevin Shih <kevin.j.shih@gmail.com>
2012-02-20 22:53:39 -0600

    added -Inf dc bias learner

---
+++ src/javaboost/conditioning/IsNegInfinityConditional.java
@@ -0,0 +1,18 @@
+package javaboost.conditioning;
+
+public final class IsNegInfinityConditional implements Conditional{
+    int m_featColumn;
+    public IsNegInfinityConditional(int featColumn) {
+	m_featColumn = featColumn;
+    }
+    public boolean isValid(float[] val) {
+	if(Float.isInfinite(val[m_featColumn])) {
+	    return true;
+	}
+	return false;
+    }
+
+    public String toString() {
+	return new String("[col:"+m_featColumn + "==-Inf]");
+    }
+}
