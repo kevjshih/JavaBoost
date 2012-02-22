0c2cf8878a21dc4d9a93d552888205559a03350a
Kevin Shih <kevin.j.shih@gmail.com>
2012-02-21 21:31:33 -0500

    implemented learners for fixed gmm

---
+++ src/javaboost/weaklearning/FixedGaussianMixtureModelClassifier.java
@@ -0,0 +1,43 @@
+package javaboost.weaklearning;
+
+import javaboost.mixturemodels.*;
+
+public final class FixedGaussianMixtureModelClassifier implements WeakClassifier{
+    private GaussianMixtureModel m_positiveModel = null;
+    private GaussianMixtureModel m_negativeModel = null;
+
+    private double m_posConf;
+    private double m_negConf;
+
+    public FixedGaussianMixtureModelClassifier(double posConf,
+					       double negConf,
+					       GaussianMixtureModel pos,
+					       GaussianMixtureModel neg) {
+	m_positiveModel = pos;
+	m_negativeModel = neg;
+	m_posConf = posConf;
+	m_negConf = negConf;
+    }
+
+    public double[] classify(float[][] data) {
+	double[] output = new double[data.length];
+	for(int i = 0; i < data.length; ++i) {
+	    double posProb = m_positiveModel.getPDF(data[i]);
+	    double negProb = m_negativeModel.getPDF(data[i]);
+	    if(posProb > negProb) {
+		output[i] = m_posConf;
+	    }else { // equal or neg
+		output[i] = m_negConf;
+	    }
+	}
+	return output;
+    }
+
+    public String toString() {
+	String description = "FixedGMM Positive:" + m_posConf +
+	    "Negative: " + m_negConf;
+	return description;
+    }
+
+
+}
