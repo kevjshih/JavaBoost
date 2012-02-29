dc654e4b5256988377e7c48c0c7c4bd97f720c74
Kevin Shih <kevin.j.shih@gmail.com>
2012-02-28 23:06:02 -0600

    classifiers now serializable

--- src/javaboost/weaklearning/WeakClassifier.java
+++ src/javaboost/weaklearning/WeakClassifier.java
@@ -1,6 +1,7 @@
 package javaboost.weaklearning;
+import java.io.Serializable;
 
-public interface WeakClassifier{
+public interface WeakClassifier extends Serializable{
     double[] classify(float[][] data);
 
 }
